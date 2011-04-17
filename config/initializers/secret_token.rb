3a172419a0c6e53191fff5fbe9029cc75156b782
Douglas Van Hollen <dsubnet0@gmail.com>
2011-04-17 13:54:24 -0400

    initial check-in after create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a0809a17511d5333a136404dd217a93b54cb49ae4fde61172d54e5584d19bfef8e1e697e9d15c905787536140fc02adeae6afd8961e60857a12e5d8ce54c0d1d'
